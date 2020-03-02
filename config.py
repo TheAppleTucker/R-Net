ac39d83f02f222381279c120e1f9c2de1024db18
William Xi <haowen@DN0a1f4199.SUNet>
2020-03-02 15:49:44 -0800

    update filter

--- config.py
+++ config.py
@@ -8,9 +8,9 @@ flags = tf.flags
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 home = os.path.expanduser("~")
-train_file = os.path.join(home, "data", "squad", "train-v1.1.json")
-dev_file = os.path.join(home, "data", "squad", "dev-v1.1.json")
-test_file = os.path.join(home, "data", "squad", "dev-v1.1.json")
+train_file = os.path.join(home, "data", "squad", "train-v2.0.json")
+dev_file = os.path.join(home, "data", "squad", "dev-v2.0.json")
+test_file = os.path.join(home, "data", "squad", "dev-v2.0.json")
 glove_word_file = os.path.join(home, "data", "glove", "glove.840B.300d.txt")
 
 target_dir = "data"
@@ -72,6 +72,7 @@ flags.DEFINE_integer("char_dim", 8, "Embedding dimension for char")
 
 flags.DEFINE_integer("para_limit", 400, "Limit length for paragraph")
 flags.DEFINE_integer("ques_limit", 50, "Limit length for question")
+flags.DEFINE_integer("ans_limit", 30, "Limit length for answer")
 flags.DEFINE_integer("test_para_limit", 1000,
                      "Max length for paragraph in test")
 flags.DEFINE_integer("test_ques_limit", 100, "Max length of questions in test")
