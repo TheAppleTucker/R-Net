7233454061df0eadc0e9d59d3657af2a28befd9e
William Xi <haowen@DN0a1f4199.SUNet>
2020-03-02 23:49:53 -0800

    asdf

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def test(config):
             qa_id, loss, yp1, yp2 = sess.run(
                 [model.qa_id, model.loss, model.yp1, model.yp2])
             answer_dict_, remapped_dict_ = convert_tokens(
-                eval_file, qa_id.tolist(), yp1.tolist(), yp2.tolist())
+                eval_file, qa_id.tolist(), yp1.tolist(), yp2.tolist(), config.use_squad_v2)
             answer_dict.update(answer_dict_)
             remapped_dict.update(remapped_dict_)
             losses.append(loss)
