3bc49ccfc20f40d205aca7de1735884df76a4dcc
William Xi <haowen@DN0a1f4199.SUNet>
2020-03-03 01:12:05 -0800

    asdf

--- util.py
+++ util.py
@@ -36,11 +36,11 @@ def get_record_parser(config, is_test=False):
         if config.use_squad_v2:
             ones = tf.ones([1], tf.int32)
             print("SHAPES:", tf.shape(ones), tf.shape(context_idxs))
-            context_idxs = tf.concat([ones, context_idxs])
-            ques_idxs = tf.concat([ones, context_idxs])
+            context_idxs = tf.concat([ones, context_idxs], 0)
+            ques_idxs = tf.concat([ones, context_idxs], 0)
             ones = tf.ones([1, char_limit], tf.int32)
-            context_char_idxs = tf.concat([ones, context_char_idxs])
-            ques_char_idxs = tf.concat([ones, ques_char_idxs])
+            context_char_idxs = tf.concat([ones, context_char_idxs], 0)
+            ques_char_idxs = tf.concat([ones, ques_char_idxs], 0)
             
         return context_idxs, ques_idxs, context_char_idxs, ques_char_idxs, y1, y2, qa_id
     return parse
