ec3d1b9056397e36241ce883df184d7aef2b37b4
William Xi <haowen@A180702.local>
2020-03-11 01:28:21 -0400

    asdf

--- download.sh
+++ download.sh
@@ -3,8 +3,8 @@
 # Download SQuAD
 SQUAD_DIR=~/data/squad
 mkdir -p $SQUAD_DIR
-wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v1.1.json -O $SQUAD_DIR/train-v1.1.json
-wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v1.1.json -O $SQUAD_DIR/dev-v1.1.json
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/train-v2.0.json -O $SQUAD_DIR/train-v2.0.json
+wget https://rajpurkar.github.io/SQuAD-explorer/dataset/dev-v2.0.json -O $SQUAD_DIR/dev-v2.0.json
 
 # Download GloVe
 GLOVE_DIR=~/data/glove
